79423d7930ab84220e1a617734a945800a4c00be
Leandro Tula <leandrotula@gmail.com>
2020-04-20 20:37:19 -0300

    Added gin gonic implementation

--- app/app.go
+++ app/app.go
@@ -1,15 +1,17 @@
 package app
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/leandrotula/golangmicroservice/controllers"
-	"net/http"
 )
+
+var ginHttp = gin.Default()
 
 func StartApp() {
 
-	http.HandleFunc("/users", controllers.GetUser)
+	ginHttp.GET("/user/:id", controllers.GetUser)
 
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+	if err := ginHttp.Run(":8081"); err != nil {
 		panic(err)
 	}
 
