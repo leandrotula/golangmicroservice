011e3891d39c6c6943c1816bb05b9a19e289f340
Leandro Tula <leandro.tula@proveedor.ues21.edu.ar>
2020-05-03 21:22:17 -0300

    Added bind correction and controller's unit tests

--- src/api/controller/github_repository_controller.go
+++ src/api/controller/github_repository_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"github.com/leandrotula/golangmicroservice/src/api/errorApi"
 	"github.com/leandrotula/golangmicroservice/src/api/repository"
 	"github.com/leandrotula/golangmicroservice/src/api/service"
@@ -11,7 +12,7 @@ import (
 func CreateRepo(c *gin.Context) {
 
 	var request repository.ApiRequest
-	if bindError := c.ShouldBind(&request); bindError != nil {
+	if bindError := c.ShouldBindBodyWith(&request, binding.JSON); bindError != nil {
 
 		errors := errorApi.NewBadRequestError("invalid json body")
 		c.JSON(errors.Status(), errors)
