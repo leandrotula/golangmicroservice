8fe46f50fddb0bf58f16925f325e29662e3d5c00
Leandro Tula <leandro.tula@proveedor.ues21.edu.ar>
2020-05-05 20:53:16 -0300

    initial version of concurrent api

--- src/api/repository/api_model_response.go
+++ src/api/repository/api_model_response.go
@@ -1,7 +1,20 @@
 package repository
 
+import "github.com/leandrotula/golangmicroservice/src/api/errorApi"
+
 type ApiResponse struct {
 	ID       int    `json:"id"`
 	Name     string `json:"name"`
 	FullName string `json:"full_name"`
+}
+
+type CreateReposResponse struct {
+	StatusCode int `json:"status_code"`
+	Results []CreateRepositoriesResponse `json:"results"`
+}
+
+type CreateRepositoriesResponse struct {
+
+	Response *ApiResponse `json:"response"`
+	Error errorApi.ApiError `json:"error"`
 }
