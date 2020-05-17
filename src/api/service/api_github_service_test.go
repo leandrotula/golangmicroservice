a7c1883ed4dddf93667bd71228d28678ff2c7b91
Leandro Tula <leandro.tula@proveedor.ues21.edu.ar>
2020-05-17 19:04:01 -0300

    Improve coverage of function that uses go routines

--- src/api/service/api_github_service_test.go
+++ src/api/service/api_github_service_test.go
@@ -90,7 +90,7 @@ func TestErrorCreateRepoOk(t *testing.T) {
 		HttpMethod: http.MethodPost,
 		Response:   &http.Response{
 			Body: ioutil.NopCloser(strings.NewReader("{\"id\":1296269,\"node_id\":\"MDEwOlJlcG9zaXRvcnkxMjk2MjY5\",\"name\":\"Hello-World\",\"full_name\":\"octocat/Hello-World\",\"owner\":{\"login\":\"octocat\",\"id\":1,\"node_id\":\"MDQ6VXNlcjE=\",\"avatar_url\":\"https://github.com/images/errorApi/octocat_happy.gif\",\"gravatar_id\":\"\",\"url\":\"https://api.github.com/users/octocat\",\"html_url\":\"https://github.com/octocat\",\"followers_url\":\"https://api.github.com/users/octocat/followers\",\"following_url\":\"https://api.github.com/users/octocat/following{/other_user}\",\"gists_url\":\"https://api.github.com/users/octocat/gists{/gist_id}\",\"starred_url\":\"https://api.github.com/users/octocat/starred{/owner}{/repo}\",\"subscriptions_url\":\"https://api.github.com/users/octocat/subscriptions\",\"organizations_url\":\"https://api.github.com/users/octocat/orgs\",\"repos_url\":\"https://api.github.com/users/octocat/repos\",\"events_url\":\"https://api.github.com/users/octocat/events{/privacy}\",\"received_events_url\":\"https://api.github.com/users/octocat/received_events\",\"type\":\"User\",\"site_admin\":false}}")),
-			StatusCode: http.StatusOK,
+			StatusCode: http.StatusCreated,
 		},
 		Err:        nil,
 	})
@@ -105,5 +105,120 @@ func TestErrorCreateRepoOk(t *testing.T) {
 	assert.NotNil(t, response)
 	assert.Nil(t, err)
 	assert.EqualValues(t, 1296269, response.ID)
+
+}
+
+func TestCreateSingleRepoInvalidRequest(t *testing.T) {
+
+	request := repository.ApiRequest{
+		Name:        "",
+		Description: "",
+	}
+
+	output := make(chan repository.CreateRepositoriesResponse)
+	service := createRepoImpl{}
+
+	go service.createSingleRepo(request, output)
+
+	result := <- output
+	assert.NotNil(t, result)
+	assert.Nil(t, result.Response)
+	err := result.Error
+	assert.EqualValues(t, http.StatusBadRequest, err.Status())
+
+}
+
+func TestCreateSingleRepoInvalidGithubResponse(t *testing.T) {
+
+	client.RestoreMockup()
+	client.AddMockBehavior(client.Mock{
+		HttpMethod: http.MethodPost,
+		Url: "https://api.github.com/user/repos",
+		Response:   &http.Response{
+			Body: ioutil.NopCloser(strings.NewReader("{\"message\":\"Requires authentication\",\"documentation_url\":\"https://developer.github.com/v3/repos/#create\"}")),
+			StatusCode: http.StatusUnauthorized,
+		},
+		Err:        nil,
+	})
+
+	request := repository.ApiRequest{
+		Name:        "test_name",
+		Description: "test_repos",
+	}
+
+	output := make(chan repository.CreateRepositoriesResponse)
+	service := createRepoImpl{}
+
+	go service.createSingleRepo(request, output)
+
+	result := <- output
+	assert.NotNil(t, result)
+	assert.Nil(t, result.Response)
+	err := result.Error
+	assert.EqualValues(t, http.StatusUnauthorized, err.Status())
+
+}
+
+func TestCreateSingleRepoNotProcessableEntity(t *testing.T) {
+
+	client.RestoreMockup()
+	client.AddMockBehavior(client.Mock{
+		HttpMethod: http.MethodPost,
+		Url: "https://api.github.com/user/repos",
+		Response:   &http.Response{
+			Body: ioutil.NopCloser(strings.NewReader("{\"message\":\"Repository creation failed.\",\"errors\":[{\"resource\":\"Repository\",\"code\":\"custom\",\"field\":\"name\",\"message\":\"name already exists on this account\"}],\"documentation_url\":\"https://developer.github.com/v3/repos/#create\"}")),
+			StatusCode: http.StatusUnprocessableEntity,
+		},
+		Err:        nil,
+	})
+
+	request := repository.ApiRequest{
+		Name:        "test_name",
+		Description: "test_repos",
+	}
+
+	output := make(chan repository.CreateRepositoriesResponse)
+	service := createRepoImpl{}
+
+	go service.createSingleRepo(request, output)
+
+	result := <- output
+	assert.NotNil(t, result)
+	assert.Nil(t, result.Response)
+	err := result.Error
+	assert.EqualValues(t, http.StatusBadRequest, err.Status())
+
+}
+
+func TestCreateSingleRepoOk(t *testing.T) {
+
+	client.RestoreMockup()
+	client.AddMockBehavior(client.Mock{
+		Url:        "https://api.github.com/user/repos",
+		HttpMethod: http.MethodPost,
+		Response:   &http.Response{
+			Body: ioutil.NopCloser(strings.NewReader("{\"id\":1296269,\"node_id\":\"MDEwOlJlcG9zaXRvcnkxMjk2MjY5\",\"name\":\"Hello-World\",\"full_name\":\"octocat/Hello-World\",\"owner\":{\"login\":\"octocat\",\"id\":1,\"node_id\":\"MDQ6VXNlcjE=\",\"avatar_url\":\"https://github.com/images/errorApi/octocat_happy.gif\",\"gravatar_id\":\"\",\"url\":\"https://api.github.com/users/octocat\",\"html_url\":\"https://github.com/octocat\",\"followers_url\":\"https://api.github.com/users/octocat/followers\",\"following_url\":\"https://api.github.com/users/octocat/following{/other_user}\",\"gists_url\":\"https://api.github.com/users/octocat/gists{/gist_id}\",\"starred_url\":\"https://api.github.com/users/octocat/starred{/owner}{/repo}\",\"subscriptions_url\":\"https://api.github.com/users/octocat/subscriptions\",\"organizations_url\":\"https://api.github.com/users/octocat/orgs\",\"repos_url\":\"https://api.github.com/users/octocat/repos\",\"events_url\":\"https://api.github.com/users/octocat/events{/privacy}\",\"received_events_url\":\"https://api.github.com/users/octocat/received_events\",\"type\":\"User\",\"site_admin\":false}}")),
+			StatusCode: http.StatusCreated,
+		},
+		Err:        nil,
+	})
+
+	request := repository.ApiRequest{
+		Name:        "test_name",
+		Description: "test_repos",
+	}
+
+	output := make(chan repository.CreateRepositoriesResponse)
+	service := createRepoImpl{}
+
+	go service.createSingleRepo(request, output)
+
+	result := <- output
+	assert.NotNil(t, result)
+	assert.Nil(t, result.Error)
+	assert.NotNil(t, result.Response)
+	assert.EqualValues(t, "Hello-World", result.Response.Name)
+	assert.EqualValues(t, 1296269, result.Response.ID)
+	assert.EqualValues(t, "octocat/Hello-World", result.Response.FullName)
 
 }
